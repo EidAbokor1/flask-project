24d09b7a0db667e6dc83dd234d782fd80e87346b
EidAbokor1 <abokoreid1@gmail.com>
2025-01-18 16:12:19 +0000

    flask-project

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_item():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_item = Item(name=name)
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_item.html')
+
+@app.route('/delete/<int:id>')
+def delete_item(id):
+    item = Item.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
